c3b3c111a082d2f86b4a8cc1a93f9ae1110f0410
jerab8 <79828479+jerab8@users.noreply.github.com>
2025-10-27 12:14:08 +0100

    vytvorene reseni pocitani kruhu

--- lekce6/kruh.cpp
+++ lekce6/kruh.cpp
@@ -1,26 +1,80 @@
-#include <include>
+#include <iostream>
 
 class Kruh
 {
    private:
 
       float radius;
+
+   public:
+
+      Kruh()
+      {
+         radius = 0.0f;
+      }
+
+      Kruh( float r )
+      {
+         setRadius( r );
+      }
+
+      void setRadius( float r )
+      {
+         if ( r < 0.0f )
+         {
+            radius = 0.0f;
+         }
+         else
+         {
+            radius = r;
+         }
+      }
+
+      float getRadius() const
+      {
+         return radius;
+      }
+
+      float spocitejPlochu() const
+      {
+         const float pi = 3.14159f;
+         return pi * radius * radius;
+      }
+
+      float spocitejObvod() const
+      {
+         const float pi = 3.14159f;
+         return 2.0f * pi * radius;
+      }
+
+      void vypisUdaje() const
+      {
+         std::cout << "Polomer kruhu: " << radius << std::endl;
+         std::cout << "Plocha kruhu: " << spocitejPlochu() << std::endl;
+         std::cout << "Obvod kruhu: " << spocitejObvod() << std::endl;
+      }
+};
+
+// BONUS: radius kruhu bude sablonovy parametr, a udela pole kruhu a pro kazdy kruh spocita vse
+
+template<int RADIUS>
+Kruh vytvorKruh()
+{
+   Kruh kruh;
+   kruh.setRadius( static_cast<float>( RADIUS ) );
+   return kruh;
 }
 
 int main()
 {
-   float prvniRadius = 2;
-   Kruh prvniKruh;
-   prvniKruh.setRadius( prvniRadius );
 
-   float druhyRaidus = 4;
-   Kruh druhyKruh( druhyRaidus );
-   std::cout << "Plocha kruhu:" << prvniKruh.spocitejPlochu() << std::stdl;
-   std::cout << "Obvod kruhu:" << prvniKruh.spocitejObvod() << std::stdl;
+   Kruh poleKruhu[] = { vytvorKruh<2>(), vytvorKruh<3>(), vytvorKruh<4>() };
 
-   druhyKruh.vypisUdaje();
+   for ( const Kruh &kruh : poleKruhu )
+   {
+      kruh.vypisUdaje();
+   }
 
-   // BONUS: radius kruhu bude sablonovy parametr, a udela pole kruhu a pro kazdy kruh spocita vse
 
    return 0;
 }
